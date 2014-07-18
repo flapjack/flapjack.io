9e3222914d288100cd0f34370a8731453dbeb5f4
Sarah Kowalik <sarah.kowalik@bulletproof.net>
2014-07-18 20:41:14 +1000

    Actually active our live reloader

--- config.rb
+++ config.rb
@@ -17,6 +17,8 @@ activate :syntax
 activate :relative_assets
 set :relative_links, true
 
+activate :livereload
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
