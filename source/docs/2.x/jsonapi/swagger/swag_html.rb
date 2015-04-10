1bb226fff44d36d3d07ba5bb30df6d4086e4c81e
Ali Graham <ali.graham@bulletproof.net>
2015-04-10 13:47:16 +0930

    bugfixes, more work on swagger template

--- source/docs/2.x/jsonapi/swagger/swag_html.rb
+++ source/docs/2.x/jsonapi/swagger/swag_html.rb
@@ -1,21 +1,23 @@
 
 # temporary hack -- will be integrated into the slate docs soon
 
-require 'swagger-core'
+require 'forwardable'
+require 'swagger'
 require 'erb'
 
-swagger_dir = File.join(File.dirname(__FILE__), 'swagger')
+swagger_dir = File.dirname(__FILE__)
 
 swagger = Swagger.load(File.join(swagger_dir, 'api.json'))
 
-template_path = ERB.load(File.join(swagger_dir, 'method.html.erb')
+template_path = File.join(swagger_dir, 'method.html.erb')
 template = ERB.new(File.read(template_path), nil, '-')
 
 output = ""
 
-[:get, :post, :patch, :delete].do |method|
+[:get, :post, :patch, :delete].each do |method|
 
   ['/contacts'].each do |path|
+
     unless swagger.paths.has_key?(path)
       STDERR.puts "No path for #{path}"
       next
